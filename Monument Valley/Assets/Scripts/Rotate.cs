eeae3c21eaa40b22fae3907ded13479fe5af64da
Tommiee <tomdepom1999@gmail.com>
2018-01-19 09:15:06 +0100

    slide + rotate scripts

--- Monument Valley/Assets/Scripts/Rotate.cs
+++ Monument Valley/Assets/Scripts/Rotate.cs
@@ -4,27 +4,74 @@ using UnityEngine;
 
 public class Rotate : MonoBehaviour {
 
-    private bool rotatingClockwise = false;
+    private float _sensitivity = 1f;
+    public float minAngle, maxAngle, midAngle;
+    public float _angleCorrection = 10f;
+    private Vector3 _mouseReference , _mouseOffset;
+    private Vector3 _rotation = Vector3.zero;
+    private bool _isRotating;
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		if(rotatingClockwise && (transform.eulerAngles.z > 270 || transform.eulerAngles.z <= 0))
+        if (_isRotating)
         {
-            transform.Rotate(0, 0, -1);
-        }
-        else if (!rotatingClockwise && transform.eulerAngles.z > 0)
-        {
-            transform.Rotate(0, 0, 1);
-        }
+            _mouseOffset = (Input.mousePosition - _mouseReference);
+            _rotation.z = -(_mouseOffset.y) * _sensitivity;
 
-        if (Input.anyKeyDown)
-        {
-            rotatingClockwise = !rotatingClockwise;
+            if (transform.eulerAngles.z < minAngle)
+            {
+                print("rotating to " + minAngle);
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, minAngle));
+            } 
+            if (transform.eulerAngles.z > maxAngle)
+            {
+                print("rotating to " + maxAngle);
+                transform.rotation = Quaternion.Euler(new Vector3(0, 0, maxAngle));
+            }
+            if (transform.eulerAngles.z + _rotation.z >= minAngle && transform.eulerAngles.z + _rotation.z <= maxAngle)
+            {
+                transform.Rotate(_rotation);
+            }
+
+            _mouseReference = Input.mousePosition;
         }
 	}
+
+    void OnMouseDown()
+    {
+        _isRotating = true;
+        _mouseReference = Input.mousePosition;
+    }
+
+    void OnMouseUp()
+    {
+        _isRotating = false;
+
+        
+        print("angle on liftup: " + transform.eulerAngles.z);
+        
+        if (transform.eulerAngles.z > maxAngle - _angleCorrection)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, maxAngle));
+            print("autocorrecting to maxAngle = " + maxAngle);
+        }
+
+        else if (transform.eulerAngles.z < minAngle + _angleCorrection)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, minAngle));
+            print("autocorrecting to minAngle = " + minAngle);
+            print("angle after correction: " + transform.eulerAngles.z);
+        }
+
+        else if(transform.eulerAngles.z > midAngle - _angleCorrection && transform.eulerAngles.z < midAngle + _angleCorrection)
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, midAngle));
+            print("autocorrecting to midAngle = " + midAngle);
+        }
+    }
 }
---
+++ Monument Valley/Assets/Scripts/Slide.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slide : MonoBehaviour {
+
+    private float _sensitivity = 0.01f;
+    public float minY, maxY;
+    private float _slideCorrection = 1f;
+    private Vector3 _mouseReference, _mouseOffset;
+    private Vector3 _slide = Vector3.zero;
+    private bool _isSliding;
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (_isSliding)
+        {
+            _mouseOffset = (Input.mousePosition - _mouseReference);
+            _slide.y = (_mouseOffset.y) * _sensitivity;
+
+            if (transform.position.y < minY)
+            {
+                print("sliding to " + minY);
+                transform.position = new Vector3(transform.position.x, minY, transform.position.z);
+            }
+            if (transform.position.y > maxY)
+            {
+                print("sliding to " + maxY);
+                transform.position = new Vector3(transform.position.x, maxY, transform.position.z);
+            }
+            if (transform.position.y + _slide.y >= minY && transform.position.y + _slide.y <= maxY)
+            {
+                transform.position = new Vector3(transform.position.x, transform.position.y + _slide.y, transform.position.z);
+            }
+
+            _mouseReference = Input.mousePosition;
+        }
+    }
+
+    void OnMouseDown()
+    {
+        _isSliding = true;
+        _mouseReference = Input.mousePosition;
+    }
+
+    void OnMouseUp()
+    {
+        _isSliding = false;
+
+
+        print("position on liftup: " + transform.position.y);
+
+        if (transform.position.y > maxY - _slideCorrection)
+        {
+            transform.position = new Vector3(transform.position.x, maxY, transform.position.z);
+            print("autocorrecting to maxY = " + maxY);
+        }
+
+        if (transform.position.y < minY + _slideCorrection)
+        {
+            transform.position = new Vector3(transform.position.x, minY, transform.position.z);
+            print("autocorrecting to minY = " + minY);
+        }
+    }
+}
