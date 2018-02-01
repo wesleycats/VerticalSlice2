27b859cc3bde8d63887d42f0972fd58c378963c2
Tommiee <tomdepom1999@gmail.com>
2018-02-01 11:49:33 +0100

    merge errors fixed

---
+++ Monument Valley/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class PlayerController : MonoBehaviour {
+
+	public List<GameObject> walkTargets;
+
+	private void Awake()
+	{
+		walkTargets = GameObject.FindGameObjectsWithTag("WalkTarget").ToList();
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+	
+	/*GameObject CheckClosestTarget()
+	{
+		GameObject closest;
+
+		
+
+		return closest;
+	}*/
+}
---
+++ Monument Valley/Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+	
+	Ray ray;
+	RaycastHit hit;
+	List<GameObject> path;
+
+	// Use this for initialization
+	void Start()
+	{
+	}
+
+	// Update is called once per frame
+	void Update()
+	{
+		/*
+		if (Input.GetMouseButtonDown(0))
+		{
+			ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		}
+		else
+		{
+			ray.origin = new Vector3(0, 0, 0);
+		}
+
+		if (Physics.Raycast(ray, out hit, 100))
+		{
+			//Debug.DrawLine(ray.origin, hit.point, Color.green);
+			if (hit.transform.tag == "WalkNode")
+			{
+				//Debug.Log(FindPath(hit.transform.gameObject.GetComponent<Node>()));
+			}
+		}*/
+	}
+}
