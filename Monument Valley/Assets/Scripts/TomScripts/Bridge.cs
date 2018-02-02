d9906485321eb2bff4009d6567382050c5dde706
Tommiee <tomdepom1999@gmail.com>
2018-02-02 15:44:16 +0100

    separated Toms scripts

---
+++ Monument Valley/Assets/Scripts/TomScripts/Bridge.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bridge : MonoBehaviour
+{
+    public bool bridgeTrigger;
+    public GameObject bridge;
+    public float targetY,stepSpeed;
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(other);
+            bridgeTrigger = true;
+        
+    }
+
+    void Update()
+    {
+        if (bridgeTrigger)
+        {
+            bridge.transform.position = Vector3.MoveTowards(bridge.transform.position,new Vector3(bridge.transform.position.x,targetY,bridge.transform.position.z),stepSpeed);
+        }
+    }
+}
